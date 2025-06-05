43874f2268b332e04a93fbe2f2297270f6fb6200
toms74209200 <motomotomato@gmail.com>
2025-06-06 01:12:58 +0900

    :white_check_mark: Update test

--- src/test/java/com/example/demo/service/OrderServiceTest.java
+++ src/test/java/com/example/demo/service/OrderServiceTest.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
@@ -12,9 +11,6 @@ import static org.mockito.Mockito.when;
 import com.example.demo.domain.Order;
 import com.example.demo.domain.OrderType;
 import com.example.demo.domain.Product;
-import com.example.demo.exception.InsufficientStockException;
-import com.example.demo.exception.InvalidOrderException;
-import com.example.demo.exception.OrderFailedException;
 import com.example.demo.model.OrderRequest;
 import com.example.demo.repository.OrderRepository;
 import com.example.demo.repository.ProductRepository;
@@ -61,16 +57,8 @@ public class OrderServiceTest {
         OrderService orderService = new OrderService(productRepositoryMock, orderRepositoryMock);
 
         // Act: 注文する
-        Order order;
-        try {
-            order =
-                    orderService.processOrder(
-                            new OrderRequest(expectedProductCode, expectedQuantity));
-        } catch (InvalidOrderException | InsufficientStockException | OrderFailedException e) {
-            e.printStackTrace();
-            fail();
-            return;
-        }
+        Order order =
+                orderService.processOrder(new OrderRequest(expectedProductCode, expectedQuantity));
 
         assertThat(order.id()).isNotBlank();
         // Assert: 注文の商品コードが一致するか
@@ -120,16 +108,8 @@ public class OrderServiceTest {
         OrderService orderService = new OrderService(productRepositoryMock, orderRepositoryMock);
 
         // Act: 注文する
-        Order order;
-        try {
-            order =
-                    orderService.processOrder(
-                            new OrderRequest(expectedProductCode, expectedQuantity));
-        } catch (InvalidOrderException | InsufficientStockException | OrderFailedException e) {
-            e.printStackTrace();
-            fail();
-            return;
-        }
+        Order order =
+                orderService.processOrder(new OrderRequest(expectedProductCode, expectedQuantity));
 
         assertThat(order.id()).isNotBlank();
         // Assert: 注文の商品コードが一致するか
